f985faa69aaa3a119104dede6caef0c9715d624d
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-11 15:03:27 +0800

    Updated dependency.

--- enyof.support.js
+++ enyof.support.js
@@ -34,10 +34,11 @@
               			"file": "enyof.js",
               			"module": "enyof",
               			"author": "Richeve S. Bebedor",
-              			"contributors": [
-              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
-              			],
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/enyof.git",
               			"test": "enyof-test.js",
               			"global": true
